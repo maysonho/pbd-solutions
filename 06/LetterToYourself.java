bae241a53a021365a1711a879ee2f1f5ee2569cb
maysonho <36411187+maysonho@users.noreply.github.com>
2018-02-21 09:17:51 -0500

    pbd-solution 06

---
+++ 06/LetterToYourself.java
@@ -0,0 +1,18 @@
+public class LetterToYourself 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("+---------------------------------------------------------+");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("|                              UNKNOWN                    |");
+        System.out.println("|                              1 Microsoft Way            |");
+        System.out.println("|                              Redmond, WA 98104          |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("+---------------------------------------------------------+");
+    }
+}
