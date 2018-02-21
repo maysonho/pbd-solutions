17578f44ccfd98c3847fbb03b1ed12c1567df79c
maysonho <36411187+maysonho@users.noreply.github.com>
2018-02-21 09:26:57 -0500

    pbd-solution 07

---
+++ 07/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials 
+{
+    public static void main(String[] args) 
+    {
+              System.out.println("JJJJJ  BBBB   PPPP");
+              System.out.println("  J    B   B  P   P");
+              System.out.println("  J    B   B  P   P");
+              System.out.println("  J    BBBB   PPPP");
+              System.out.println("J J    B   B  P ");
+              System.out.println("J J    B   B  P ");
+              System.out.println(" JJ    BBBB   P");
+    }
+}
