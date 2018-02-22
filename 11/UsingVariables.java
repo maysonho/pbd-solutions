10f96884fc2a2c6cc078a904be6cd7980a41dff9
maysonho <36411187+maysonho@users.noreply.github.com>
2018-02-22 09:02:10 -0500

    pbd-solution 11

---
+++ 11/UsingVariables.java
@@ -0,0 +1,17 @@
+public class UsingVariables
+{
+    public static void main(String[] args)
+    {
+        int Roomnumber;
+        double Enumber;
+        String Coursename;
+        
+        Roomnumber = 113;
+        Enumber = 2.71828;
+        Coursename = "computer science";
+        
+        System.out.println("This is room # " + Roomnumber + ".");
+        System.out.println("e is close to " + Enumber + ".");
+        System.out.println("I am learning a bit about " + Coursename + ".");
+    }
+}
