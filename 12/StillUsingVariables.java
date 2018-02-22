72af66e7e1ba453000e82304b9123a60c88ff50e
maysonho <36411187+maysonho@users.noreply.github.com>
2018-02-22 09:12:37 -0500

    pbd-solution 12

---
+++ 12/StillUsingVariables.java
@@ -0,0 +1,14 @@
+public class StillUsingVariables
+{
+    public static void main(String[] args)
+    {
+        int Year;
+        String Name;
+        
+        Year = 4000;
+        Name = "John";
+        
+        System.out.println("My name is " + Name + " and I'll graduate in " + Year + ".");
+    }
+}
+
