930d934af1d29f0ba467bba730a7fed998c675f7
maysonho <36411187+maysonho@users.noreply.github.com>
2018-02-22 09:23:49 -0500

    pbd-solution 13

---
+++ 13/YourSchedule.java
@@ -0,0 +1,20 @@
+public class YourSchedule
+{
+    public static void main(String[] args)
+    {
+        String course1, course2, course3, teacher1, teacher2, teacher3;
+        
+        course1 = "English";
+        course2 = "Math";
+        course3 = "Science";
+        teacher1 = " Mr. Sarracini";
+        teacher2 = "Ms. Koon";
+        teacher3 = "Mr. Yeghiaian";
+        
+        System.out.println("+-----------------------------------------------------------------+");
+        System.out.println("| 1 |                              " + course1 + " |        " + teacher1 + " |");
+        System.out.println("| 2 |                              " + course2 + " |        " + teacher2 + " |");
+        System.out.println("| 3 |                              " + course3 + " |        " + teacher3 + " |");
+        System.out.println("+-----------------------------------------------------------------+");
+    }
+}
